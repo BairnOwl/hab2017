3be50eab6a56217f1a5521ee5554172996f3ed27
npolshakova <nina_polshakova@brown.edu>
2017-02-04 20:03:01 -0500

    I did lots of stuff

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ app.get('/', function (req, res) {
     res.render('index.html');
 });
 
+app.get('/src/:file', function (req, res) {
+    res.render('src/'+req.params.file);
+});
+
+app.get('/bower_components/:folder/:file', function (req, res) {
+    res.render('bower_components/'+req.params.folder+'/'+req.params.file);
+});
+
 // USER ROUTES
 
 // Creates a new user
